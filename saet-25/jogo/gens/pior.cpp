de5b8145ddcda572cc74d03522177a42ab5c564b
Ricardo T Oliveira <reiracofage@gmail.com>
2025-10-08 20:40:57 -0300

    J: Adicionado mais um pior caso

---
+++ saet-25/jogo/gens/pior.cpp
@@ -0,0 +1,16 @@
+#include "testlib.h"
+
+using namespace std;
+
+typedef pair<int,int> ii;
+typedef pair<int, ii > i3;
+
+int main(int argc, char *argv[]) {
+    registerGen(argc, argv, 1);
+
+    printf("3\n");
+    printf("1 1 1\n");
+    printf("2 1 1\n");
+    printf("30 12 1000000000\n");
+
+}
